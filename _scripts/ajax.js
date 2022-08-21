718f063bc0fdb2b7e45a66674db32355765aee74
templar.command0 <templar.command0@gmail.com>
2022-08-21 21:06:20 +0530

    add api key in _scripts/ajax.js

--- _scripts/ajax.js
+++ _scripts/ajax.js
@@ -1,17 +1,25 @@
 // add global variable containing XHR object here
-
+let httpRequest = new XMLHttpRequest();
 
 // add get() function here
-
+function get(url, success, fail) {
+  httpRequest.open("GET", url);
+  httpRequest.onload = () => {
+    httpRequest.status === 200
+      ? success(httpRequest.responseText)
+      : fail(httpRequest.status);
+  };
+  httpRequest.send();
+}
 
 function tempToF(kelvin) {
-    return ((kelvin - 273.15) * 1.8 + 32).toFixed(0);
+  return ((kelvin - 273.15) * 1.8 + 32).toFixed(0);
 }
 
 function successHandler(data) {
-    const dataObj = JSON.parse(data);
-    const weatherDiv = document.querySelector('#weather');
-    const weatherFragment = `
+  const dataObj = JSON.parse(data);
+  const weatherDiv = document.querySelector("#weather");
+  const weatherFragment = `
         <h1>Weather</h1>
         <h2 class="top">
         <img
@@ -22,16 +30,30 @@ function successHandler(data) {
         />${dataObj.name}
         </h2>
         <p>
-        <span class="tempF">${tempToF(dataObj.main.temp)}&deg;</span> | ${dataObj.weather[0].description}
+        <span class="tempF">${tempToF(dataObj.main.temp)}&deg;</span> | ${
+    dataObj.weather[0].description
+  }
         </p>
-    `
-    weatherDiv.innerHTML = weatherFragment;
-    weatherDiv.classList.remove('hidden');
+    `;
+  weatherDiv.innerHTML = weatherFragment;
+  weatherDiv.classList.remove("hidden");
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    const apiKey = ''; // ADD YOUR API KEY BETWEEN THE QUOTES
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=los+angeles&APPID=' + apiKey;
-    // add get() function call here
-    
+function failHandler(status) {
+  console.error(status, "error occurred while fetching data from server");
+
+  const weatherDiv = document.querySelector("#weather");
+
+  weatherDiv.classList.remove("hidden");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const apiKey = "07ebd59e1632937e4c8edff407b3e123"; // ADD YOUR API KEY BETWEEN THE QUOTES
+
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=los+angeles&APPID=" +
+    apiKey;
+  // add get() function call here
+  get(url, successHandler, failHandler);
+  // successHandler(httpRequest.responseText); // instead use callback in get function
 });
