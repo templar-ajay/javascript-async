760b90760db77d5952b83bb6cfbaf8e5c0ff9d27
templar-command0 <templar.command0@gmail.com>
2022-08-22 13:50:19 +0530

    add promises polyfill, update readme.md

--- README.md
+++ README.md
@@ -57,3 +57,38 @@ get()
   });
 // calls the successHandler and failureHandler events asynchronously after getting the response from the httpRequest
 ```
+
+## `Promise.all([])` method
+
+```js
+// make sure you provide an array of requests inside Promise.all() -> Promise.all([a,b,c,d])
+Promise.all([fetch(urls[0]), fetch(urls[1]), fetch(urls[2])]).then(
+  (responses) => {
+    return responses.map((response) => {
+      return response.json();
+    })
+  }.then(jsonResponses=>{
+    return jsonResponses.join(",")
+  }).then(data=>{
+    console.log(data)
+  }).catch(err=>{
+    console.error(err)
+  }).finally(()=>{
+    console.log('all fetch requests have returned json data`)
+  })
+);
+```
+
+## Promises Polyfill - making code backwards compatible
+
+promises are not supported in older browsers like IE and opera browsers , and promises can not be transpiled using Babel
+
+- so we use Polyfills to transpile the Promises into older syntax of js for old browsers support
+- to use a polyfill in your code insert the following cdn in html before the script that uses promises
+  one of the promise polyfill is `https://github.com/taylorhakes/promise-polyfill`
+
+cdn -
+
+```html
+<script src="https://cdn.jsdelivr.net/npm/promise-polyfill@8/dist/polyfill.min.js"></script>
+```
--- _scripts/ajax.js
+++ _scripts/ajax.js
@@ -20,8 +20,7 @@ function tempToF(kelvin) {
 
 function successHandler(data) {
   const dataObj = JSON.parse(data);
-  const weatherDiv = document.querySelector("#weather");
-  const weatherFragment = `
+  const div = `
         <h1>Weather</h1>
         <h2 class="top">
         <img
@@ -37,14 +36,11 @@ function successHandler(data) {
   }
         </p>
     `;
-  weatherDiv.innerHTML = weatherFragment;
-  weatherDiv.classList.remove("hidden");
+  return div;
 }
 
 function failHandler(status) {
   console.error(status, "error occurred while fetching data from server");
-
-  const weatherDiv = document.querySelector("#weather");
 
   weatherDiv.classList.remove("hidden");
 }
@@ -52,12 +48,40 @@ function failHandler(status) {
 document.addEventListener("DOMContentLoaded", function () {
   const apiKey = "07ebd59e1632937e4c8edff407b3e123"; // ADD YOUR API KEY BETWEEN THE QUOTES
   // const apiKey = "";
+  const weatherDiv = document.querySelector("#weather");
 
-  const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=los+angeles&APPID=" +
-    apiKey;
+  const locations = [
+    "los+angeles,us",
+    "san+francisco,us",
+    "lone+pine,us",
+    "mariposa,us",
+  ];
+
+  // const url =
+  //   "https://api.openweathermap.org/data/2.5/weather?q=los+angeles&APPID=" +
+  //   apiKey;
+
+  const urls = locations.map((location) => {
+    return `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiKey}`;
+  });
   // add get() function call here
   // successHandler(httpRequest.responseText); // instead use callback in get function
   // get(url, successHandler, failHandler);
-  console.log(get(url));
+  // console.log(get(url));
+
+  Promise.all([get(urls[0]), get(urls[1]), get(urls[2]), get(urls[3])])
+    .then((responses) => {
+      return responses.map((response) => {
+        return successHandler(response);
+      });
+    })
+    .then((literals) => {
+      weatherDiv.innerHTML = `<h1>Weather</h1>${literals.join("")}`;
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+    .finally(() => {
+      weatherDiv.classList.remove("hidden");
+    });
 });
